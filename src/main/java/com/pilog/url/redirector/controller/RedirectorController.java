c9ba15e6de4c08103177656524f78fdcae81848d
vishal <vishalpundir@outlook.com>
2024-03-12 10:44:09 +0530

    swagger config added

--- src/main/java/com/pilog/url/redirector/controller/RedirectorController.java
+++ src/main/java/com/pilog/url/redirector/controller/RedirectorController.java
@@ -9,17 +9,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping
 public class RedirectorController {
 
     @Autowired private IAIService iaiService;
 
-    @GetMapping("/getSuitableRes")
-    public ResponseEntity<?> showAITypedValueResults(
+    @GetMapping(path="/api/v1/getSuitableRes")
+    public ResponseEntity<?> getSuitableRes(
             @RequestParam("prompt") String prompt,
             @RequestParam(value = "apiType", defaultValue = "gpt") APIType apiType) {
         String response = getResponse(apiType, prompt);
